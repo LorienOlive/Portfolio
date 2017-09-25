034c5f258cefc02ead9e9aeafd0f8b5aa5c14807
Lorien Olive <lorien.olive@gmail.com>
2017-09-24 23:29:13 -0400

    tests new stylng for breakpoints

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 
 import '../styles/Home.css'
 
+import splash from '../styles/images/webpage-home2.png'
+
 class Home extends Component {
   render() {
     return (
       <div className="home-container" id="homePage">
-        <div className="home-image">
+        <img className="home-image" src={splash} alt="weird collage" />
+        <div className="home-header-container">
           <h1 className="home-header">Lorien Olive</h1>
           <h3 className="home-subheader">Front-End Web Developer</h3>
         </div>
       </div>
+
     )
   }
 }
